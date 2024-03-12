989add2091e3d895d604a20343d451e237d6a493
JohnnyBackus <john.p.backus@gmail.com>
2024-03-12 14:58:21 -0700

    assign projects via element

--- sunbursts/views_front.py
+++ sunbursts/views_front.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Project, Element, Survey
+from django.shortcuts import render
 
 
 class ElementListView(LoginRequiredMixin, ListView):
@@ -14,6 +15,10 @@ class SurveyView(LoginRequiredMixin, CreateView, UpdateView):
     model = Survey
     fields = "__all__"
     context_object_name = "survey"
+    def survey_view(request, survey_id):
+        survey_instance = Survey.objects.get(pk=survey_id)
+        return render(request, 'survey.html', {'survey_instance': survey_instance})
+
     # success_url = reverse_lazy("thank_you")
     # lookup_field = 'unique_link'
 
