80f9e1422b5ac3c2883463fd04c8cb41cf940bea
Xin Deng <xind1470@gmail.com>
2024-03-12 14:32:14 -0700

    added graph url

--- sunbursts/urls_front.py
+++ sunbursts/urls_front.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views_front import ElementListView, SurveyView
+from .views_front import ElementListView, SurveyView, GraphListView
 urlpatterns = [
     path("elements/", ElementListView.as_view(), name="element_list"),
     path("survey/<int:pk>", SurveyView.as_view(), name="survey"),
     # path("survey/<uuid:unique_link>", survey_form, name="survey_form"),
+    path('graph/', GraphListView.as_view(), name='graph'),
+
 ]
