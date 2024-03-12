4208448694b25982e3f2e1ed08aaba22f65fe09e
JohnnyBackus <john.p.backus@gmail.com>
2024-03-12 12:24:41 -0700

    updated models

--- sunbursts/models.py
+++ sunbursts/models.py
@@ -18,28 +18,35 @@ class Participant(models.Model):
         return self.participant_name
 
 class Element(models.Model):
-    project_name = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True)
+    # project_name = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True)
+    survey = models.ForeignKey('Survey', on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=255, blank=True, null=True)
     category = models.CharField(max_length=25, blank=True, null=True)
     def __str__(self) -> Any:
         return self.name
 
-class Response(models.Model):
+class Survey(models.Model):
     project_name = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True)
+    # selected_elements = models.ManyToManyField(Element)
+    user_selections_max = models.IntegerField(default=10)
+    weighting_max = models.IntegerField(default=20)
+    readiness_score_max = models.IntegerField(default=10)
+    def __str__(self) -> Any:
+        return self.project_name.name
+
+class SurveyResponse(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, null=True, blank=True)
+    # responses = models.ForeignKey(Response, on_delete=models.CASCADE, null=True, blank=True)
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self) -> Any:
+        return self.survey.project_name
+
+class ElementResponse(models.Model):
     element = models.ForeignKey(Element, on_delete=models.CASCADE)
+    survey_response = models.ForeignKey(SurveyResponse, on_delete=models.CASCADE, null=True, blank=True)
     weighting_input = models.IntegerField(default=0)
     trend_input = models.IntegerField(default=0)
     readiness_input = models.IntegerField(default=0, blank=True, validators=[MinValueValidator(0), MaxValueValidator(10)])
     def __str__(self) -> Any:
-        return self.participant
-
-class Survey(models.Model):
-    project_name = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True)
-    selected_elements = models.ManyToManyField(Element)
-    user_selections_max = models.IntegerField(default=10)
-    readiness_score_max = models.IntegerField(default=10)
-    weighting_max = models.IntegerField(default=20)
-    def __str__(self) -> Any:
-        return self.project_name.name
+        return self.element.name
