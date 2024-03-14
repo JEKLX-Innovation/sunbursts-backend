ba7eec784c0b95861442636a7e78fb9f752df12d
KaitlinDa <Kaitlin.A.Davis1996@Gmail.com>
2024-03-14 17:48:41 -0500

    adds unique link generation and paths to survey

--- sunbursts/urls_front.py
+++ sunbursts/urls_front.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views_front import ElementTableView, SurveyView, GraphListView, SurveyResponseView
+from .views_front import survey_for_participant
 
 urlpatterns = [
     path("elements/", ElementTableView.as_view(), name="element_table"),
-    path("survey/<int:pk>/", SurveyView.as_view(), name="survey"),
-    # path("survey/<uuid:unique_link>", survey_form, name="survey_form"),
     path('graph/', GraphListView.as_view(), name='graph'),
     path('survey_response/', SurveyResponseView.as_view(), name='survey_response'),
     path('thank_you/', GraphListView.as_view(), name='thank_you'),
+    path('survey/<uuid:unique_link>/', survey_for_participant, name='survey_for_participant'),
 ]
