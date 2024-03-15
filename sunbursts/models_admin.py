345d4ae11574f9732e2441de0e991ba1a2eb6399
JohnnyBackus <john.p.backus@gmail.com>
2024-03-14 20:29:51 -0700

    attempt at csv.DictReader

--- sunbursts/models_admin.py
+++ sunbursts/models_admin.py
@@ -4,7 +4,7 @@ from django.urls import path
 from django.shortcuts import render, redirect
 import csv
 import io
-from .models import SunburstElement
+from .models import SunburstElement, Participant
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -23,20 +23,30 @@ class ProjectAdmin(admin.ModelAdmin):
             csv_form = CSVImportForm(request.POST, request.FILES)
             if csv_form.is_valid():
                 project = csv_form.cleaned_data['project']
+                # participant = csv_form.cleaned_data['participant']
                 csv_file = request.FILES['csv_file']
                 data_set = csv_file.read().decode('UTF-8')
                 io_string = io.StringIO(data_set)
-                next(io_string)
-                for row in csv.reader(io_string, delimiter=',', quotechar='"'):
+                reader = csv.DictReader(io_string)
+                for row in reader:
+                    print(row)
+                    if not row.get('Element Name') or not row.get('Point Score') or not row.get('Need Score') or not row.get('Score') or not row.get('Category') or not row.get('Participant Name'):
+                        print("Skipping row due to missing data")
+                        continue
+
                     _, created = SunburstElement.objects.update_or_create(
                         project=project,
-                        element_name=row[0],
-                        point_score=row[1],
-                        need_score=row[2],
-                        score=row[3],
-                        category=row[4],
-
+                        element_name=row['Element Name'],
+                        point_score=row['Point Score'],
+                        need_score=row['Need Score'],
+                        score=row['Score'],
+                        category=row['Category'],
                     )
+                    _, created = Participant.objects.update_or_create(
+                        participant_name=row['Participant Name'],
+                        participant_email=row['Participant Email']
+                    )
+
                 messages.success(request, "Your CSV file has been imported")
                 return redirect("..")
         else:
